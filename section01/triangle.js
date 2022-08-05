f1e9709106e953c148267e5e2aa15ef8b31543f4
Dongha922 <kdh4575@naver.com>
2022-08-05 13:33:49 +0900

    [study] section01의 7/17강 까지

--- section01/triangle.js
+++ section01/triangle.js
@@ -1,12 +1,14 @@
-
-function solution(a, b, c){
-    let answer="YES", max;
-    let tot=a+b+c;
-    if(a>b) max=a;
-    else max=b;
-    if(c>max) max=c;
-    if(tot-max<=max) answer="NO"; 
+function sol(a,b,c){
+    //a+b>c 1. max 값 찾기
+    //2. yes, no로 출력 
+    let answer="Yes", max;
+    let sum = a+b+c;
+    if(a>b) max= a;
+    else max = b;
+    if(c>max) max= c;
+    if(max<(sum-max)) answer = "Yes";
+    else answer = "No";
     return answer;
-}
 
-console.log(solution(13, 33, 17));
+}
+console.log(sol(13,33,17));
